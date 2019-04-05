0ec11b041584ffddad8ac4a02aa53d16cbc058c1
Matthew Caldwell <MCaldwell4242@gmail.com>
2019-04-04 20:24:56 -0500

    got it working

--- main.js
+++ main.js
@@ -1,1 +1,54 @@
-console.log('testing');
+
+let places = [];
+
+const printToDom = (divId, textToPrint) => {
+    const selectedDiv = document.getElementById(divId);
+    selectedDiv.innerHTML = textToPrint;
+};
+
+const domStringBuilder = (arrayToPrint) => {
+    let domString = '';
+    arrayToPrint.forEach((place) => {
+        domString += `<div class="card">`;
+            domString += `<img src="${place.cityImage}" class="card-img-top">`;
+                domString += `<div class="card-body">`;
+                domString += `<h3 class="card-title">${place.cityName}</h4>`;
+                domString += `<h4>${place.cityState}</h4>`;
+                domString += `<h5>${place.favoriteRestaurant}</h5>`;
+                domString += `<h5>${place.favoriteBar}</h5>`;
+                domString += `<h5>${place.favoriteHotel}</h5>`;
+                domString += `<h5>${place.favoriteTouristAttraction}</h5>`;
+                domString += `</div>`;
+        domString += `</div>`;    
+    });
+        printToDom('faves', domString);
+};
+
+
+function executeThisCodeAfterFileLoads(){
+    const data = JSON.parse(this.responseText);
+    places = data.places;
+    domStringBuilder(data.places);
+};
+
+
+function executeThisCodeifXHRFails(){
+    console.error('oh shit');
+};
+
+const getPlaceData = () => {
+    const myRequest = new XMLHttpRequest();
+    myRequest.addEventListener('load', executeThisCodeAfterFileLoads);
+    myRequest.addEventListener('error', executeThisCodeifXHRFails);
+    myRequest.open('GET', './db/places.json');
+    myRequest.send();
+    console.log(myRequest);
+
+};
+
+
+const init = () => {
+    getPlaceData();
+};
+
+init();
